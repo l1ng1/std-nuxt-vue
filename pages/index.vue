5222f2497af8d284e47f3cf4c6a3f25189e83c80
l1ng1 <pk9942601@gmail.com>
2025-07-25 12:36:20 +0600

    1 try

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <div>
+        <h1>Привет, мир!</h1>
+        <p>Это пример файла Vue на Options API.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Добро пожаловать в Vue Options API!'
+        }
+    },
+    methods: {
+        greet() {
+            alert(this.message);
+        }
+    },
+    mounted() {
+        this.greet();
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
